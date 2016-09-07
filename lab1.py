0469f793e12fb0ed34926924840b7666c59797aa
megsum <msumner@ualberta.ca>
2016-09-07 15:13:36 -0600

    downloads itself and displays the code

--- lab1.py
+++ lab1.py
@@ -1,4 +1,4 @@
 import requests
 
-response = requests.post('http://ccid-eddieantonio.rhcloud.com/msumner')
-print response.status_code
+response = requests.get('https://github.com/megsum/Cmput401Lab1/raw/master/lab1.py')
+print response.text
